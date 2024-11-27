8f3c8c36023413dd74ac0f1dfad8638bb5580ea7
Lucas Dev <albaishere.contact@gmail.com>
2024-11-27 20:55:11 +0100

    Partie Discord normalement finie

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 /* ********************************************* */
 /*                                               */
 /*                 Partie Discord                */
-/*         Ce n'est pas du tout terminé !        */
 /*                                               */
 /* ********************************************* */
 
 require('dotenv').config();
 const { token } = process.env;
 const { Client, Collection, Partials } = require('discord.js');
+const { SearchOnThisDay } = require('./retrieve_wikipedia');
 const {User, Message, GuildMember, ThreadMember, Channel, Reaction, GuildScheduledEvent} = Partials;
 const channelid = "1240001870865498184";
+const roleid = "1167616651265577003";
 
 const client = new Client({ intents: '3276799', partials: [User, Message, GuildMember, ThreadMember, Channel, Reaction, GuildScheduledEvent ] });
 
@@ -33,37 +34,38 @@ function Top3(data) {
 client.on('ready', async() => {
     console.log(`${client.user.username} is ready !`)
 
-    const ladatedujour = { /* ladatedujour.json */ }
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Intl.DateTimeFormat('fr-FR', options).format(date);
+    const options2 = { day: 'numeric', month: 'long' };
+    const formattedDate2 = new Intl.DateTimeFormat('fr-FR', options2).format(date);
+
+    const channel = await client.channels.fetch(channelid);
+    const ladatedujour = await SearchOnThisDay()
+
+    const topEvenements = ladatedujour.elementshistoriques
+        .sort((a, b) => b.year - a.year)
+        .slice(0, 4)
+        .sort((a, b) => a.year - b.year)
+        .map(evenements => `**${evenements.year}** : ${evenements.text}`)
+        .join('\n\n');
+
+    const topMorts = Top3(ladatedujour.deaths)
+        .map(mort => `**${mort.year}** : ${mort.name} ${mort.description} *( Popularité: ${mort.popularity} )*`)
+        .join('\n\n');
+
+    const topNaissances = Top3(ladatedujour.births)
+        .map(naissance => `**${naissance.year}** : ${naissance.name} ${naissance.description} *( Popularité: ${naissance.popularity} )*`)
+        .join('\n\n');
+
+    const topFetes = ladatedujour.holidays
+        .map(fete => `${fete.text}`)
+        .join('\n\n');
 
     const millisecondes = tempsavantminuit();
+
     setTimeout(async () => {
-        const date = new Date();
-        const options = { day: 'numeric', month: 'long', year: 'numeric' };
-        const formattedDate = new Intl.DateTimeFormat('fr-FR', options).format(date);
-        const options2 = { day: 'numeric', month: 'long' };
-        const formattedDate2 = new Intl.DateTimeFormat('fr-FR', options2).format(date);
-
-        console.log(`Nous sommes le ${formattedDate} ! <a:cat:1310685205547323432>`);
-        console.log(`:tada: Bon anniversaire à ceux qui sont nés un ${formattedDate2} ! :tada:`)
-        const channel = await client.channels.fetch(channelid);
-
-        const topEvenements = ladatedujour.evenements_historiques
-            .sort((a, b) => a.annee - b.annee)
-            .map(evenements => `**${evenements.annee}** : ${evenements.text}`)
-            .join('\n\n');
-
-        const topMorts = Top3(ladatedujour.morts)
-            .map(mort => `**${mort.annee}** : ${mort.nom} ${mort.description} *( Popularité: ${mort.popularite} )*`)
-            .join('\n\n');
-
-        const topNaissances = Top3(ladatedujour.naissances)
-            .map(naissance => `**${naissance.annee}** : ${naissance.nom} ${naissance.description} *( Popularité: ${naissance.popularite} )*`)
-            .join('\n\n');
-
-        const topFetes = ladatedujour.fetes
-            .map(fete => `${fete.text}`)
-            .join('\n\n');
-
         await channel.send(`# Nous sommes le ${formattedDate} ! <a:cat:1310685205547323432>\n
 ## Bon anniversaire à ceux qui sont nés un ${formattedDate2} ! :tada:\n
 
@@ -86,13 +88,13 @@ ${topFetes}
 
 ### - Sources :
 
-Événements historiques, Anniversaires, Décès, Fêtes et Journées Internationales : Wikipédia.
-Popularité : Google Trends
+Tout : [Wikipédia](<https://fr.wikipedia.org/wiki/>)
+Popularité : [Google Trends](<https://trends.google.fr/trends/>)
 
 
 **Envoyé par : <@${client.user.id}>**
 
-<@&1167616651265577003>`)
+||<@&${roleid}>||`)
     }, millisecondes)
 })
 
